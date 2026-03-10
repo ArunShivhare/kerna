d4ea8888300d6340de339857c7874e384c9efb2d
Arun Shivhare <arunshivhare925@gmail.com>
2026-03-10 10:04:32 +0530

    Initial Commit

---
+++ app/careers/page.js
@@ -0,0 +1,7 @@
+export default function Careers() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Careers at Kerna</h1>
+    </main>
+  );
+}
---
+++ app/work/page.js
@@ -0,0 +1,7 @@
+export default function Work() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Our Work</h1>
+    </main>
+  );
+}
